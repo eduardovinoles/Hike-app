c2e06db2302f9dc9453b98caeb03e72d0913603d
Eduardo <evinoles@bootia.dev>
2023-08-04 15:21:50 -0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by eduardo vinoles on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
